69618c012d6e8f392dbd09bf225361c940510f91
Khaled <fk_hamidi@esi.dz>
2020-04-10 06:04:53 +0100

    Implementing serverless functions

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,20 +3,38 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+//import Axios from 'axios' ..
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDEzCCAfugAwIBAgIJOetjyNmo/s5gMA0GCSqGSIb3DQEBCwUAMCcxJTAjBgNV
+BAMTHHVkYWdyYW0taGFiaWJhbGxhaC5hdXRoMC5jb20wHhcNMjAwMzA0MTkzNjU0
+WhcNMzMxMTExMTkzNjU0WjAnMSUwIwYDVQQDExx1ZGFncmFtLWhhYmliYWxsYWgu
+YXV0aDAuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn+sit9gY
+7fsfuZBHKR4ZBlDvYPLhzKF/irWp/bCStLiSv1Q638vCIeNhFO0aG/CfaaaXKHRB
+4fR12Cjm6270WIqASXekqbSWAEh1laMekAFcESHQ3JrRGgMQ1cpDk915610S0Zje
+wPNoYAB6WxZ1J3TXEy6aDbm/5qcDJ16kfK0gEmuE/W3n4mhb+zKdxFQkVrwvDaxf
+jlZOv2JvPLlwkfGcpMaNCVm6JynMRDu7LkSHtEvGLYm8LNsW+7zg6T+UFyULIL6g
+9kRdbyxck0iWG2HcL3h+A5FwYpYhH2HTQt0vJTjeQdjhxKJwkNk7CPaf/VMMCPn4
+cgBvOmiJOGVxfwIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBR8
+dqVnwrgtmA/rpjHAGkRFyyBRtzAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQEL
+BQADggEBAFbwRsKUJfMJ2fN20k0w7qxtWNpzDGJehkesXjTwgva5/2GLdzLrML7t
+eJ15vmbgf+GcOHsuztfPnem4QdLToobjxj1GTBkXxHP/aoAxM6ERdrhVRCVuA0gI
+KPyAO8ML2uex/pFWWJrY63mLxZjy4pRiYubclJ24i9bgtPPMu7eh1MAjWShyq6gm
+CLgpj7eeODIihCHhvyiCwsPu5kjz4lLdiqNy++DcwR0vz+dcVxPqEanszsN7UDkW
+B6qqgqdSo3Jyeb3bQvvzSBqZC57nk2ZVLQLvaIDWViTnpFfi+aTKAGP1NvPi+9Ny
+4wBRAAunWP+Md/lKE1VoJy4P+sI2SFE=
+-----END CERTIFICATE-----`
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
-    const jwtToken = await verifyToken(event.authorizationToken)
+    const jwtToken = await  verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -33,7 +51,7 @@ export const handler = async (
       }
     }
   } catch (e) {
-    logger.error('User not authorized', { error: e.message })
+    logger.error('User not authorized', { error: e })
 
     return {
       principalId: 'user',
@@ -51,12 +69,25 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  return undefined
+  if(!jwt){
+    throw new Error('invalid token')
+  }
+
+  try {
+    //const response = await Axios.get(jwksUrl);
+    //console.log(response);
+    var verifedToken = verify(token,cert,{algorithms:['RS256']})
+
+    logger.info('verfied toekn',verifedToken)
+    return  verifedToken as JwtPayload
+  } catch (error) {
+    console.error(error);
+    return undefined
+  }
 }
 
 function getToken(authHeader: string): string {
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,11 +1,38 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId} from '../../helpers/authHelper'
+import { TodosAccess } from '../../dataLayer/todosAccess'
+import { ApiResponseHelper } from '../../helpers/apiResponseHelper'
+import { createLogger } from '../../utils/logger'
 
+const todosAccess = new TodosAccess()
+const apiResponseHelper = new ApiResponseHelper()
+const logger = createLogger('todos')
+//..
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
+    if(!todoId){
+        logger.error('invalid deletion attempt without todo id')
+        return apiResponseHelper.generateErrorResponse(400,'invalid parameters')
+    }
+ 
+    const authHeader = event.headers['Authorization']
+    const userId = getUserId(authHeader)
 
-    // TODO: Remove a TODO item by id
-    return undefined
-  }
-)
+    const item = await todosAccess.getTodoById(todoId)
+    if(item.Count == 0){
+        logger.error(`user ${userId} requesting deletion for non existing todo with id ${todoId}`)
+        return apiResponseHelper.generateErrorResponse(400,'TODO does not exist')
+    }
+
+    if(item.Items[0].userId !== userId){
+        logger.error(`user ${userId} requesting delete todo does not belong to this account with id ${todoId}`)
+        return apiResponseHelper.generateErrorResponse(400,'TODO does not belong to the authorized user')
+    }
+
+    logger.info(`User ${userId} deleting todo ${todoId}`)
+    await todosAccess.deleteTodoById(todoId)
+    return apiResponseHelper.generateEmptySuccessResponse(204)
+
+  
+}
