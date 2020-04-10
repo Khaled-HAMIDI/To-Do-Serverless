686e75415c96188a41006b504d9b421da0ef33d7
Khaled <fk_hamidi@esi.dz>
2020-04-10 06:16:57 +0100

    Configuring the frontend

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '...'
+const apiId = '...' // I could't deploy the project so i have not a api id
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-ljim07sw.auth0.com',
+  clientId: 'A0ZQlDGTx0eG5zknYBJQjp6LYb0xwhyX',
   callbackUrl: 'http://localhost:3000/callback'
 }
